5846101b28f189e6dcac46e20296b396e428d9cf
GranthSidhant <sidhant2granth@gmail.com>
2025-04-15 00:10:33 +0530

    coded alert.js

---
+++ js/alert.js
@@ -0,0 +1,22 @@
+function logout() {
+    alert("You have Been Logged Out");
+  }
+
+function acc_delete() {
+    alert("Your Account has been Deleted");
+  }
+
+function logout() {
+    alert("You have Been Logged Out");
+  }
+
+function contact(){
+    alert("Thank You!! Your Response has been submitted");
+
+  }
+
+function profileUpdate(){
+    alert("Your Profile Has been Updated");
+    // window.location.href = 'account.html';
+    
+  }
